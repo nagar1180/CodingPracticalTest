d8bca6f9ed4166d59cbc3242290b4c22f6f90214
nagar1180 <52356541+nagar1180@users.noreply.github.com>
2021-08-26 16:11:32 +0530

    Added new repo

---
+++ Controllers/DomainTraceController.cs
@@ -0,0 +1,54 @@
+﻿using CodingPracticalTest.Model.Request;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CodingPracticalTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DomainTraceController : ApiControllerBase
+    {
+        private readonly ILogger<DomainTraceController> logger;
+        public DomainTraceController(ILogger<DomainTraceController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpPost, Route("")]
+        public async Task<IActionResult> Index([FromBody] IEnumerable<RequestModel> model)
+        {
+            try
+            {  
+                if (model.Count() > 0)
+                {
+                    List<Task<string>> allTasks = new List<Task<string>>();
+                    HttpClient client = new HttpClient();
+                    foreach (var item in model)
+                    {
+                        if (ModelState.IsValid)
+                        {
+                            var task = client.GetStringAsync(item.Address);
+                            allTasks.Add(task);
+                        }                      
+                    }
+                    
+                    string[] result = await Task.WhenAll(allTasks);
+                    return Ok(result);
+                }
+                return BadRequest("At least one address should provide");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message, ex);
+                return StatusCode(500, "Internal server error : Please contact api administrator");
+            }            
+        }       
+    }
+}
---
+++ Model/Request/RequestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodingPracticalTest.Model.Request
+{
+    public class RequestModel //: IValidatableObject
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage ="Ip address or domain is required.")]
+        public string Address { get; set; }     
+    }
+}
